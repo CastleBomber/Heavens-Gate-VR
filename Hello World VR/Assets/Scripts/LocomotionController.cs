df255e86d3803a1c97ed46c267912cfb3bc066b4
CastleBomber <richie.ucsb@gmail.com>
2022-06-11 09:55:59 -0700

    post capstone and freed up lfs storage space

---
+++ Hello World VR/Assets/Scripts/LocomotionController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class LocomotionController : MonoBehaviour
+{
+    public XRController leftTeleportRay;
+    public XRController rightTeleportRay;
+    public InputHelpers.Button teleportActivationButton;
+    public float activationThreshold;
+
+    // Update is called once per frame
+    void Update()
+    {
+		if (leftTeleportRay)
+		{
+            leftTeleportRay.gameObject.SetActive(CheckIfActivated(leftTeleportRay));
+		}
+
+		if (rightTeleportRay)
+		{
+            rightTeleportRay.gameObject.SetActive(CheckIfActivated(rightTeleportRay));
+		}
+
+
+    }
+
+    public bool CheckIfActivated(XRController controller)
+	{
+        //teleportActivationButton = InputHelpers.Button.PrimaryButton; // sweeeett!!!! makes use of 'A', 'x' button vs trigger 
+        InputHelpers.IsPressed(controller.inputDevice, teleportActivationButton, out bool isActivated, activationThreshold);
+        return isActivated;
+	}
+}
